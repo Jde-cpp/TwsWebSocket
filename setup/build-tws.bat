0302f05890bfd41ddfd29f13c2adcd1a0e3b571e
jde-cpp <johnduffyx@outlook.com>
2022-02-20 14:17:35 -0500

    tws to 2022

--- setup/build-tws.bat
+++ setup/build-tws.bat
@@ -1,9 +1,12 @@
 Set TwsPath=C:\TWS API\source\CppClient\client
+Set mypath=%~dp0
+echo %mypath:~0,-1%
+copy "%mypath:~0,-1%\TwsSocketClient64.vcxproj" "%TwsPath%\TwsSocketClient64.vcxproj"
 Set SubDir=BuildTools
-echo done
-set x="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%SubDir%\MSBuild\Current\Bin\"
-echo x;
-if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%SubDir%\MSBuild\Current\Bin\" ( set SubDir=Enterprise)
+rem echo done
+set x="%ProgramFiles(x86)%\Microsoft Visual Studio\2022\%SubDir%\MSBuild\Current\Bin\"
+rem echo x;
+if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\%SubDir%\MSBuild\Current\Bin\" ( set SubDir=Enterprise)
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%SubDir%\MSBuild\Current\Bin\MSBuild.exe" "%TwsPath%\TwsSocketClient64.vcxproj"  -p:Configuration=Release -p:Platform=x64
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2022\%SubDir%\MSBuild\Current\Bin\MSBuild.exe" "%TwsPath%\TwsSocketClient64.vcxproj"  -p:Configuration=Release -p:Platform=x64
 copy "%TwsPath%\.bin\Release" .
