65088de9a0e4b7c008cfd27ad496a2695095c701
jde-cpp <johnduffyx@outlook.com>
2022-02-20 13:52:10 -0500

    misc.

--- source/PreviousDayValues.h
+++ source/PreviousDayValues.h
@@ -3,7 +3,7 @@
 namespace Jde::Markets::TwsWebSocket
 {
 	struct ProcessArg;
-	α Previous( int32 contractId, Markets::TwsWebSocket::ProcessArg arg )noexcept->Coroutine::Task;
+	//α Previous( int32 contractId, Markets::TwsWebSocket::ProcessArg arg )noexcept->Coroutine::Task;
 	α PreviousDayValues( ContractPK contractId )noexcept( false )->void;
 	α PreviousDayValues( const google::protobuf::RepeatedField<google::protobuf::int32>& contractIds, const ProcessArg& arg )noexcept->void;
 }
